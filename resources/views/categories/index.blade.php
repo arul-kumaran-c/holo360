3ae0f06ef6f17adfc44dde3fa7f315a5bd7cf20f
Arulkumaran C <arulkumaran.c@outlook.com>
2025-02-05 17:56:34 +0400

    category index completed

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h2>Index</h2>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Sl.No</th>
+                <th>Name</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($categories as $category)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $category->$name }}</td>
+                <td>{{ $category->$created_at }}</td>
+                <td>{{ $category->$updated_at }}</td>
+                <td>
+                   
+            </tr>
+            
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
